ecef8bf8ba011058f31775a7fc98e33038fa5072
hoseok <swiftie52@naver.com>
2024-11-14 17:23:18 +0900

    fix : lat :값  lng : 값 으로 나오게 수정

--- src/main/java/com/web/gilproject/dto/BoardDTO/BoardPathDTO.java
+++ src/main/java/com/web/gilproject/dto/BoardDTO/BoardPathDTO.java
@@ -4,28 +4,29 @@ import com.web.gilproject.domain.Path;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 public record BoardPathDTO(
         Long id,
         String title,
+        String content,
         Integer time,
         double distance,
         double startLat,
         double startLong,
-        List<double[]> route
-)
-{
-    public static BoardPathDTO from(Path path)
-    {
+        List<Map<String, Double>> route
+) {
+    public static BoardPathDTO from(Path path) {
         return new BoardPathDTO(
                 path.getId(),
                 path.getTitle(),
+                path.getContent(),
                 path.getTime(),
                 path.getDistance(),
                 path.getStartLat(),
                 path.getStartLong(),
                 Arrays.stream(path.getRoute().getCoordinates())
-                        .map(coord->new double[]{coord.x,coord.y})
+                        .map(coord -> Map.of("lat", coord.y, "lng", coord.x))
                         .toList()
         );
     }
