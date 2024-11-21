19028bc5c0d6256615d32bff8384c4b1a2ae0d7f
BOOK-1BE04K1S34\jinye <jinyerom1998@gmail.com>
2024-11-21 17:26:10 +0900

    11/21 17:25 PostResDTO 관련 생성자 오류 수정

--- src/main/java/com/web/gilproject/service/GilListServiceImpl.java
+++ src/main/java/com/web/gilproject/service/GilListServiceImpl.java
@@ -1,6 +1,7 @@
 package com.web.gilproject.service;
 
 import com.web.gilproject.domain.Post;
+import com.web.gilproject.domain.PostImage;
 import com.web.gilproject.dto.CustomUserDetails;
 import com.web.gilproject.dto.PathResDTO;
 import com.web.gilproject.dto.PostDTO_YJ.PostResDTO;
@@ -251,9 +252,13 @@ public class GilListServiceImpl implements GilListService {
 
             PathResDTO pathResDTO = pathService.decodingPath(post.getPath()); //path 형식 바꾸기
 
+            List<String> imageUrls = post.getPostImages().stream()
+                    .map(PostImage::getImageUrl)
+                    .collect(Collectors.toList());
+
             result.add(new PostResDTO(id, userNickName, pathId, startLat, startLong, state, title, content, tag,
                     writeDate,updateDate,readNum,postLikesCount, repliesCount,
-                    postWishListsNum, userImgUrl, pathResDTO));
+                    postWishListsNum, userImgUrl, pathResDTO, imageUrls));
         }
         return result;
     }
