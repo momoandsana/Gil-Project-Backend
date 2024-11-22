d8b92b9a23b2062d1f155a1142d8244b8b7520a2
hoseok <swiftie52@naver.com>
2024-11-22 15:59:44 +0900

    fix : BoardController 수정

--- src/main/java/com/web/gilproject/controller/BoardController.java
+++ src/main/java/com/web/gilproject/controller/BoardController.java
@@ -111,6 +111,7 @@ public class BoardController {
      * 게시글 상세보기
      *
      * @param postId
+     * @param authentication
      * @return
      */
     @GetMapping("/{postId}")
@@ -122,6 +123,14 @@ public class BoardController {
         return ResponseEntity.ok(postResDTO);
     }
 
+    /**
+     * 게시글 수정하기
+     *
+     * @param postId
+     * @param postPatchRequestDTO
+     * @param authentication
+     * @return
+     */
     @PatchMapping("/{postId}")
     public ResponseEntity<Void> updatePost(@PathVariable Long postId,PostPatchRequestDTO postPatchRequestDTO,Authentication authentication)
     {
