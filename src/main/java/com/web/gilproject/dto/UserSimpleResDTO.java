49f6dfdf2d74d659b455915de4244913d6f19d5a
Eunmi-Han <leahan0302@gmail.com>
2024-11-26 20:53:21 +0900

    마이페이지 정보 조회 -> 개수 조회로 변경

--- src/main/java/com/web/gilproject/dto/UserSimpleResDTO.java
+++ src/main/java/com/web/gilproject/dto/UserSimpleResDTO.java
@@ -18,9 +18,9 @@ public class UserSimpleResDTO {
     private String nickName;             //닉네임
     private String imageUrl;             //프로필 이미지
     private String comment;              //자기소개글
-    private Integer postCounts;          //내가 쓴 글 개수
-    private Integer pathCounts;          //따라걷기 개수
-    private Integer subscriptionCounts;  //구독자 수(나를 구독하는 유저 수)
+    private Integer postCount;          //내가 쓴 글 개수
+    private Integer pathCount;          //따라걷기 개수
+    private Integer subscribeByCount;  //구독자 수(나를 구독하는 유저 수)
 
     public UserSimpleResDTO(User user) {
         this.id = user.getId();
