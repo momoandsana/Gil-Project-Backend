0960ba6e81c8fec186063f571fca20d218e3d78b
JHW <jeedd95@gmail.com>
2024-11-27 10:08:50 +0900

    리프레시 토큰 DB에서 삭제 로직, 로그아웃 로직

--- src/main/java/com/web/gilproject/config/SecurityConfig.java
+++ src/main/java/com/web/gilproject/config/SecurityConfig.java
@@ -80,7 +80,7 @@ public class SecurityConfig {
                         //config.setExposedHeaders(Collections.singletonList("Set-Cookie"));
 
                         //응답시 노출할 헤더
-                        config.setExposedHeaders(Arrays.asList("authorization", "newaccess","abc"));
+                        config.setExposedHeaders(Arrays.asList("authorization", "newaccess","oauth2access","Set-Cookie"));
 
 
                         return config;
