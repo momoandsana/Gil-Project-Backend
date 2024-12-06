6c218a0664226e220911a5c40eef20f82914733f
hoseok <swiftie52@naver.com>
2024-12-06 11:43:06 +0900

    fix : action test & es

--- src/main/java/com/web/gilproject/controller/AmazonController.java
+++ src/main/java/com/web/gilproject/controller/AmazonController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RequestMapping("/api")
 public class AmazonController {
 
-    ////
+    /////
     private final AmazonService s3Service;
     //
     public AmazonController(AmazonService s3Service) {
